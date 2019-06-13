243fc6b8ea121803c881ef868781639b03828eb9
takakurakaede <takakurakaede@192.168.0.3>
2019-06-13 20:42:36 +0900

    [add]画面から入力
    3-2

--- SpringSample/src/main/java/com/example/demo/trySpring/HelloController.java
+++ SpringSample/src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo.trySpring;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -9,5 +12,11 @@ public class HelloController {
 	public String gerHello() {
 
 		return "hello";
+	}
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1")String str,Model model) {
+		model.addAttribute("sample",str);
+
+		return "helloResponse";
 	}
 }
