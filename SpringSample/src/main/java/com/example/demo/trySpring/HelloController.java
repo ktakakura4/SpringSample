871d8ad56f6d36dc27f157be5214018788af64e0
takakurakaede <takakurakaede@192.168.0.5>
2019-06-15 01:32:26 +0900

    [add]データベースから取得
    3-3

--- SpringSample/src/main/java/com/example/demo/trySpring/HelloController.java
+++ SpringSample/src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,4 +1,5 @@
 package com.example.demo.trySpring;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
+	@Autowired
+    private HelloService helloService;
 
 	@GetMapping("/hello")
 	public String gerHello() {
@@ -19,4 +23,22 @@ public class HelloController {
 
 		return "helloResponse";
 	}
+
+	@PostMapping("/hello/db")
+    public String postDbRequest(@RequestParam("text2") String str, Model model) {
+
+        // Stringからint型に変換
+        int id = Integer.parseInt(str);
+
+        // １件検索
+        Employee employee = helloService.findOne(id);
+
+        // 検索結果をModelに登録
+        model.addAttribute("id", employee.getEmployeeId());
+        model.addAttribute("name", employee.getEmployeeName());
+        model.addAttribute("age", employee.getAge());
+
+        // helloResponseDB.htmlに画面遷移
+        return "helloResponseDB";
+    }
 }
